e5d5a19e0099382dba6055f1d08ceaef09561498
jbarget <justenbarget@gmail.com>
2016-12-22 18:49:20 +0000

    adding html form post instead of frontend javascript

--- src/public/js/index.js
+++
@@ -1,7 +0,0 @@
-import request from 'app/request.js';
-
-document.getElementById('loginToGithub').addEventListener('click', () => {
-  request.get('/login')
-    .then((res) => console.log('RESULT: ', res))
-    .catch((err) => console.log('ERROR: ', err));
-});
