8d5a869cab030d2625624564d3b44bb20bbf1bd0
Sam Houston <shouston3@me.com>
2016-12-22 18:30:57 +0000

    Setup of basic promise based request library. Further config changes for rollup and set up of github auth. Fixing linting errors and setting semistandard env to browser

---
+++ src/api/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const request = require('request');
+const qs = require('querystring');
+const path = require('path');
+const app = express();
+
+require('env2')('config.env');
+
+app.use(express.static('src/public'));
+
+app.get('/build/bundle.js', (req, res) => res.sendFile(path.resolve('build/bundle.js')));
+
+app.get('/login', (req, res) => {
+  const params = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    redirect_uri: process.env.BASE_URL + '/welcome'
+  };
+  res.redirect('https://github.com/login/oauth/authorize?' + qs.stringify(params));
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
