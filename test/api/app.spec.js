2431bb7afee01a13f380705870de3dd588c03bec
Sam Houston <shouston3@me.com>
2016-12-27 13:53:40 +0000

    Adding in `done` cb to signify end of statement

--- test/api/app.spec.js
+++ test/api/app.spec.js
@@ -4,16 +4,15 @@ const shot = require('shot');
 const app = require('../../src/api/app.js');
 
 describe('Express server', () => {
-  it('/ - should respond with a 200 and an html page', () => {
+  it('/ - should respond with a 200 and an html page', (done) => {
     const request = {
       method: 'GET',
       url: '/'
     };
 
     shot.inject(app, request, (res) => {
-      console.log(res.headers['content-type']);
-      console.log(res.statusCode);
-      assert.equal(res.statusCode, 100, 'correct!');
+      assert.equal(res.statusCode, 200, 'correct!');
+      done();
     });
   });
 });
