25a8977265b35d5facc319a3ab71ee34931c0bf8
jbarget <justenbarget@gmail.com>
2016-12-29 12:08:29 +0000

    adding response to /repos/org_name

--- src/api/repos.js
+++ src/api/repos.js
@@ -13,21 +13,24 @@ const repos = (req, res) => {
 
   request.get(options, (err, response, body) => {
     if (err) {
-      console.error(err);
-
       return res.status(500).send({
         message: 'Error: ' + err,
         payload: {}
       });
     }
-    console.log('>>>>>>>>', body);
 
-    // const data = JSON.parse(body).map((repo) => ({
-    //   id: repo.id,
-    //   name: repo.name,
-    //   image: repo.avatar_url
-    // }));
-    const data = {};
+    const payload = JSON.parse(body).map((repo) => ({
+      name: repo.name,
+      open_issues_count: repo.open_issues_count,
+      last_update: repo.updated_at,
+      repo_url: repo.url
+    }));
+
+    const data = {
+      message: 'Retrieving repos for organisation: ' + req.params.org_name,
+      payload
+    };
+
     res.send(data);
   });
 };
