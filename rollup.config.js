3877f88953556af219c3c63e8a3fba455e627485
Sam Houston <shouston3@me.com>
2016-12-30 22:07:11 +0000

    not using strict for bundle for no chrome error on travis

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 export default {
   entry: 'src/public/js/index.js',
   dest: 'build/bundle.js',
+  useStrict: false,
   format: 'iife',
   moduleName: 'HiSayer',
   sourceMap: true,
