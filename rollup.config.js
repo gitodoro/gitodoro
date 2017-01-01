cb3170fb4af6d2f3e4d2b991d5e8a6fd0f2e065d
Sam Houston <shouston3@me.com>
2017-01-01 15:29:59 +0000

    Removing script to edit the bundle after transpilation and instead doing it with a rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,17 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
+import init from 'rollup-plugin-init';
 
 export default {
   entry: 'src/public/js/index.js',
-  dest: 'build/bundle.js',
+  dest: process.env.NODE_ENV === 'test' ? 'build/bundle_init.js' :'build/bundle.js',
   useStrict: false,
   format: 'iife',
-  moduleName: 'HiSayer',
+  moduleName: 'gitodoro',
   sourceMap: true,
   sourceMapFile: 'bundle/bundle.js',
   plugins: [nodeResolve({
     jsnext: true,
     main: true,
     browser: true
-  })]
+  })].concat(process.env.NODE_ENV === 'test' ? [ init() ] : [])
 };
